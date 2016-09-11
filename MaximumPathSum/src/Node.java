4be782996b616a0679a93f8784fa8097f5d92a63
mgruben <GrubenM@gmail.com>
2016-09-11 10:27:06 -0500

    Implement Node class

---
+++ MaximumPathSum/src/Node.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2016 Michael <GrubenM@GMail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *
+ * @author Michael <GrubenM@GMail.com>
+ */
+public class Node implements Comparable<Node> {
+    int num;
+    Node left;
+    Node right;
+    
+    public Node(int num) {
+        this.num = num;
+        left = null;
+        right = null;
+    }
+    
+    public int compareTo(Node other) {
+        return Integer.valueOf(this.num).compareTo(other.num);
+    }
+    
+    public String toString() {
+        return Integer.toString(num);
+    }
+}
